e275694ab18e1c71611c8727c8186ab9d14ba4ac
Ahmedhamza1232001 <ahmedhamza7895123@gmail.com>
2024-01-07 16:58:33 +0200

    Add ProductsController and Dto

---
+++ SmartShop.API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SmartShop.Core;
+
+namespace SmartShop.API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IGenericRepository<Product> _productsRepo;
+    private readonly IMapper _mapper;
+    public ProductsController(IGenericRepository<Product> productsRepo, IMapper mapper)
+    {
+        _productsRepo = productsRepo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
+    {
+        var spec = new ProductsWithSubCategoriesSpecification();
+
+        var products = await _productsRepo.ListAsync(spec);
+
+        return Ok(_mapper.Map<IReadOnlyList<ProductToReturnDto>>(products));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+    {
+        var spec = new ProductsWithSubCategoriesSpecification(id);
+
+        var product = await _productsRepo.GetEntityWithSpec(spec);
+
+        return _mapper.Map<Product, ProductToReturnDto>(product);
+    }
+}
---
+++ SmartShop.Core/Dtos/ProductToReturnDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartShop.Core;
+
+public record class ProductToReturnDto
+{
+    public required string Name { get; set; }
+    public float Rate { get; set; }
+    public bool InStock { get; set; }
+    public int Quantity { get; set; }
+    public int SubCategoryId { get; set; }
+    public required string SubCategory { get; set; }
+    public int BrandId { get; set; }
+    public required string Brand { get; set; }
+    public string Color { get; set; } = string.Empty;
+    public required string PictureUrl { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
