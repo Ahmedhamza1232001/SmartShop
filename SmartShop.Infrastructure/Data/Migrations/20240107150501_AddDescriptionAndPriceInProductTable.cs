9221973b7763ba1041cba49cf88eb6a6ec0d4a44
Ahmedhamza1232001 <ahmedhamza7895123@gmail.com>
2024-01-07 17:08:23 +0200

    Migrate the configuration and the two new columns

---
+++ SmartShop.Infrastructure/Data/Migrations/20240107150501_AddDescriptionAndPriceInProductTable.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartShop.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionAndPriceInProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Mobiles",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Mobiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Mobiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Mobiles",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Computers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Computers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Computers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Computers",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Mobiles");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Mobiles");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Mobiles");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Computers");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Computers");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Computers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Mobiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Computers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
