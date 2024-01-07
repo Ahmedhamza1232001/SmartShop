c79d50b86e1b8c9869f43ee5fca12c345199b610
Ahmedhamza1232001 <ahmedhamza7895123@gmail.com>
2024-01-07 16:08:12 +0200

    Add Description and Price to Product entity

--- SmartShop.Core/Entities/Product.cs
+++ SmartShop.Core/Entities/Product.cs
@@ -7,7 +7,7 @@ public abstract class Product : BaseEntity
 {
     public required string Name { get; set; }
     public float Rate { get; set; }
-    public bool InStock { get; set; }
+    public bool InStock { get; set; } 
     public int Quantity { get; set; }
     public int SubCategoryId { get; set; }
     public required SubCategory SubCategory { get; set; }
@@ -15,4 +15,6 @@ public abstract class Product : BaseEntity
     public required string Brand { get; set; }
     public Color Color { get; set; }
     public required string PictureUrl { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
 }
