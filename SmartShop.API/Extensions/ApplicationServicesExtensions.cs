0c9293595eebd5ff06f92eaa9b1d67fd65f5dea1
Ahmedhamza1232001 <ahmedhamza7895123@gmail.com>
2024-01-13 14:43:18 +0200

    Add Documentation Configuration
    - Add ApplicationService Extensions File
    - Add configuration for documenation
    - Add constants file to add xml file names

---
+++ SmartShop.API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using SmartShop.Core;
+using SmartShop.Infrastructure;
+using static SmartShop.Core.Constants.DocumentationConstants;
+
+namespace SmartShop.API.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config, IWebHostEnvironment hostingEnvironment)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "SmartShop",
+                });
+                // Get the current working directory
+                string apiPath = hostingEnvironment.ContentRootPath;
+
+                //var x = AppContext.BaseDirectory;
+                foreach (var xmlFile in XmlFiles)
+                {
+                    var xmlPath = Path.Combine(apiPath + DocumentationFolderPath + xmlFile);
+                    options.IncludeXmlComments(xmlPath);
+                }
+            }
+
+        });
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(
+                config.GetConnectionString("DefaultConnection"),
+                sqlServerOptions =>
+                {
+                    sqlServerOptions.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+                    // Additional SQL Server options if needed
+                    // sqlServerOptions.CommandTimeout(60);
+                }
+            );
+        });
+
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = actionContext =>
+            {
+                var errors = actionContext.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                var errorResponse = new ApiValidationErrorResponse
+                {
+                    Errors = errors
+                };
+
+                return new BadRequestObjectResult(errorResponse);
+            };
+        });
+
+
+
+        return services;
+    }
+}
+
---
+++ SmartShop.Core/Constants/DocumentationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartShop.Core.Constants;
+public static class DocumentationConstants
+{
+    public const string DocumentationFolderPath = "\\..\\SmartShop.Core\\Documentations\\";
+    public const string AuditLogAppServiceDocs = "ProductController.xml";
+
+
+    public static string[] XmlFiles = new[]
+            {
+             AuditLogAppServiceDocs
+            };
+}
