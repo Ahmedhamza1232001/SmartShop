3658acfb0b4bff815365fca65df2104a947a671f
Ahmed hamza <ahmedhamza7895123@gmail.com>
2024-01-16 15:56:09 +0200

    Add Pagination and Filtring and Sorting

--- SmartShop.Core/Specifications/ProductsWithSubCategoriesSpecification.cs
+++ SmartShop.Core/Specifications/ProductsWithSubCategoriesSpecification.cs
@@ -4,9 +4,34 @@ namespace SmartShop.Core;
 
 public class ProductsWithSubCategoriesSpecification : BaseSpecification<Product>
 {
-    public ProductsWithSubCategoriesSpecification()
+    //need to rename this to ProductsWithTypesAndBrandsSpecification
+    public ProductsWithSubCategoriesSpecification(ProductSpecParams productParams)
+     : base(x =>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
     {
         AddInclude(x => x.SubCategory);
+        AddOrderBy(x => x.Name);
+        ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1),
+                productParams.PageSize);
+
+        if (!string.IsNullOrEmpty(productParams.Sort))
+        {
+            switch (productParams.Sort)
+            {
+                case "priceAsc":
+                    AddOrderBy(p => p.Price);
+                    break;
+                case "priceDesc":
+                    AddOrderByDescending(p => p.Price);
+                    break;
+                default:
+                    AddOrderBy(n => n.Name);
+                    break;
+            }
+        }
     }
 
     public ProductsWithSubCategoriesSpecification(int id) : base(x => x.Id == id)
